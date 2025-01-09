a148a9448f3e643d1d2041bae74b3588409668f5
Nicolas Guerrero <Guerreronicolas1872@gmail.com>
2025-01-09 15:18:17 -0800

    Update Student schema to match students db model

--- src/app/schemas.py
+++ src/app/schemas.py
@@ -4,5 +4,16 @@ class ORMSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
 class StudentSchema(ORMSchema):
-    id: int
+    cti_id: int
     fname: str
+    pname: str
+    lname: str
+    join_date: str # TODO Validate date
+    target_year: int
+    gender: str
+    first_gen: bool
+    institution: str
+    is_graduate: bool
+    birthday: str # TODO Validate date
+    active: bool
+    cohort_lc: bool
