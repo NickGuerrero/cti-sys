4e1b13b46a296eb75341bbd0e880f118b5a9d1da
Nicolas Guerrero <Guerreronicolas1872@gmail.com>
2025-01-24 13:01:30 -0800

    Add relationships to intial models
    SQLAlchemy is unclear on how back_propagate behaves, and SQLITE
    is not sufficient for testing. Pushing to be tested on an actual
    Postgres installation.

--- src/app/models.py
+++ src/app/models.py
@@ -1,13 +1,19 @@
 from sqlalchemy import (
-    Integer, String, Date, Boolean, DateTime, Float, TimeStamp,
-    ForeignKey
+    Integer, String, Date, Boolean, DateTime, Float,
+    ForeignKey, ForeignKeyConstraint
 )
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime, date
+from typing import List
 from .database import Base
 
 # TODO: FKs, Composite keys, and proper table definitions
 # For PG, consider removing length limit (https://wiki.postgresql.org/wiki/Don%27t_Do_This#Don.27t_use_varchar.28n.29_by_default)
+# Composite foriegn keys: https://stackoverflow.com/questions/75747252/using-sqlalchemy-orm-with-composite-primary-keys
+
+###
+# Core Student Data: Shared across all programs
+###
 class Student(Base):
     __tablename__ = "students"
     cti_id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
@@ -23,32 +29,35 @@ class Student(Base):
     birthday: Mapped[date] = mapped_column(Date)
     active: Mapped[bool] = mapped_column(Boolean)
     cohort_lc: Mapped[bool] = mapped_column(Boolean)
+    # Relationships
+    email_addresses: Mapped[List["StudentEmail"]] = relationship(back_populates="email_owner")
+    canvas_id: Mapped["CanvasID"] = relationship(back_populates="id_owner")
+    ethnicities: Mapped["Ethnicity"] = relationship(back_populates="eth_owner")
+    attendances: Mapped[List["StudentAttendance"]] = relationship(back_populates="student")
 
 class StudentEmail(Base):
     __tablename__ = "student_emails"
     email: Mapped[str] = mapped_column(String, primary_key=True)
-    cti_id: Mapped[int] = mapped_column(Integer) # Index here
+    cti_id: Mapped[int] = mapped_column(ForeignKey("students.cti_id")) # TODO: Index here
     is_primary: Mapped[bool] = mapped_column(Boolean)
+    email_owner: Mapped["Student"] = relationship(back_populates="email_addresses")
 
 class CanvasID(Base):
     __tablename__ = "canvas_ids"
-    cti_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    cti_id: Mapped[int] = mapped_column(ForeignKey("students.cti_id"), primary_key=True)
     canvas_id: Mapped[int] = mapped_column(Integer) # Index here
+    id_owner: Mapped["Student"] = relationship(back_populates="canvas_id")
 
 class Ethnicity(Base):
     __tablename__ = "ethnicities"
-    cti_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    cti_id: Mapped[int] = mapped_column(ForeignKey("students.cti_id"), primary_key=True)
     ethnicity: Mapped[str] = mapped_column(String)
-    details: Mapped[str] = mapped_column(String) # We may need this to be Text
+    details: Mapped[str] = mapped_column(String)
+    eth_owner: Mapped["Student"] = relationship(back_populates="canvas_id")
 
-class StudentAttendance(Base):
-    __tablename__ = "student_attendance"
-    cti_id: Mapped[int] = mapped_column(Integer) # PK
-    session_id: Mapped[int] = mapped_column(Integer) # PK
-    peardeck_score: Mapped[float] = mapped_column(Float(3))
-    attended_minutes: Mapped[int] = mapped_column(Integer)
-    session_score: Mapped[float] = mapped_column(Float(3))
-
+##################################
+# Attendance Data
+##################################
 class Attendance(Base):
     __tablename__ = "attendance"
     session_id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -60,11 +69,32 @@ class Attendance(Base):
     link: Mapped[str] = mapped_column(String)
     owner: Mapped[str] = mapped_column(String)
     last_processed_date: Mapped[datetime] = mapped_column(DateTime)
+    # Relationships
+    attendances: Mapped[List["StudentAttendance"]] = relationship(back_populates="session")
+    missing_records: Mapped[List["MissingAttendance"]] = relationship(back_populates="session")
+
+class StudentAttendance(Base):
+    __tablename__ = "student_attendance"
+    cti_id: Mapped[int] = mapped_column(ForeignKey("students.cti_id"), primary_key=True) # PK
+    session_id: Mapped[int] = mapped_column(ForeignKey("attendance.session_id"), primary_key=True) # PK
+    peardeck_score: Mapped[float] = mapped_column(Float(3))
+    attended_minutes: Mapped[int] = mapped_column(Integer)
+    session_score: Mapped[float] = mapped_column(Float(3))
+    # Relationships
+    student: Mapped["Student"] = relationship(back_populates="attendances")
+    session: Mapped["Attendance"] = relationship(back_populates="attendances")
+    __table_args__ = (
+        ForeignKeyConstraint(
+            [cti_id, session_id], [Student.cti_id, Attendance.session_id]
+        ),
+    )
 
 class MissingAttendance(Base):
     __tablename__ = "missing_attendance"
-    email: Mapped[str] = mapped_column(String)
+    email: Mapped[str] = mapped_column(String, primary_key=True)
     session_id: Mapped[int] = mapped_column(Integer)
     name: Mapped[str] = mapped_column(String)
     peardeck_score: Mapped[int] = mapped_column(Float(3))
     attended_minutes: Mapped[int] = mapped_column(Integer)
+    # Relationships
+    session: Mapped["Attendance"] = relationship(back_populates="missing_records")
