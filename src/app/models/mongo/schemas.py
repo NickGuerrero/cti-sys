b883b1bfbf033ad4577afbd5e07ecd5ec82da5b1
ConlynPattison <cpattison@csumb.edu>
2025-01-31 13:47:29 -0800

    Add structure to collection def and indexing
    Commit adds class structure to the definition of a Collection as it can
    be initialized from within the project. Definition uses previously
    defined JSON schema as well as indexes for each collection. Indexes not
    currently checked/added once collections have already been found as
    present in the db.

--- src/app/models/mongo/schemas.py
+++ src/app/models/mongo/schemas.py
@@ -1,35 +1,47 @@
+from typing import Any, Dict, List
+from pymongo import IndexModel
 from pymongo.database import Database
 
 from src.config import APPLICATIONS_COLLECTION
 
-collections = {
-    APPLICATIONS_COLLECTION: {
-        "bsonType": "object",
-        "title": "Application Object Validation",
-        "required": ["email", "fname", "lname", "app_submitted"],
-        "properties": {
-            "email": {
-                "bsonType": "string",
-                "pattern": "^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$",
-                "description": "Must provide a valid email address"
+class CollectionProps:
+    def __init__(self, schema: Dict[str, Any], indexes: List[IndexModel]):
+        self.schema = schema
+        self.indexes = indexes
+
+collections: dict[str, CollectionProps] = {
+    APPLICATIONS_COLLECTION: CollectionProps(
+        schema={
+            "bsonType": "object",
+            "title": "Application Object Validation",
+            "required": ["email", "fname", "lname", "app_submitted"],
+            "properties": {
+                "email": {
+                    "bsonType": "string",
+                    "pattern": "^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$",
+                    "description": "Must provide a valid email address"
+                },
+                "fname": {
+                    "bsonType": "string",
+                    "description": "Must provide a first name of minimum length 1",
+                    "minLength": 1
+                },
+                "lname": {
+                    "bsonType": "string",
+                    "description": "Must provide a last name of minimum length 1",
+                    "minLength": 1
+                },
+                "app_submitted": {
+                    "bsonType": "date",
+                    "description": "Must provide the date at which the application was submitted as UTC datetime"
+                },
             },
-            "fname": {
-                "bsonType": "string",
-                "description": "Must provide a first name of minimum length 1",
-                "minLength": 1
-            },
-            "lname": {
-                "bsonType": "string",
-                "description": "Must provide a last name of minimum length 1",
-                "minLength": 1
-            },
-            "app_submitted": {
-                "bsonType": "date",
-                "description": "Must provide the date at which the application was submitted as UTC datetime"
-            },
+            "additionalProperties": True
         },
-        "additionalProperties": True
-    }
+        indexes=[
+            IndexModel("email", unique=True)
+        ]
+    )
 }
 
 def init_schemas(mongo: Database):
@@ -38,14 +50,18 @@ def init_schemas(mongo: Database):
     # if the collection exists, update with db.command; else create the collection
     for collection in collections:
         if collection not in exitingCollections:
+            # create the schema with the above defined JSON schema
             mongo.create_collection(
-            collection,
-            validator={"$jsonSchema": collections[collection]},
-            validationLevel="moderate" # validates on write
+                collection,
+                validator={"$jsonSchema": collections[collection].schema},
+                validationLevel="moderate" # validates on writes
             )
+            # if the collection requires indexes, include them upon creation
+            if len(collections[collection].indexes) > 0:
+                mongo.get_collection(collection).create_indexes(collections[collection].indexes)
         else:
             mongo.command({
-            "collMod": collection,
-            "validator": {"$jsonSchema": collections[collection]},
-            "validationLevel": "moderate"
-})
+                "collMod": collection,
+                "validator": {"$jsonSchema": collections[collection]},
+                "validationLevel": "moderate"
+            })
