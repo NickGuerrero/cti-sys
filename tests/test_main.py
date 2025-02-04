fa0641e94a8ab8cef94593e314e9f30a9a108c45
aleguzmancs9 <aleguzmancs9@gmail.com>
2025-02-03 17:48:13 -0800

    [TASK] Set-Up Testing with GitHub Actions #5
    * Set up pytest and add GitHub Actions workflow for testing
    
    * Add tests for root and test_connection endpoints
    
    * Added pytest dependencies to requirements.txt
    
    ---------
    Lead author: Alexis Guzman
    Very Minor Changes By: NickGuerrero <51380496+NickGuerrero@users.noreply.github.com>

---
+++ tests/test_main.py
@@ -0,0 +1,15 @@
+import pytest
+from fastapi.testclient import TestClient
+from src.main import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "cti-sys v1.0.0"}
+
+def test_confirm_conn():
+    response = client.get("/test-connection")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Database connection succeeded"}
