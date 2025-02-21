73b1a8549d6e8dd1e60f932efb8edcc3b98cf39d
ConlynPattison <cpattison@csumb.edu>
2025-02-21 13:12:49 -0800

    Add tests for accepting valid inserts to db

--- tests/test_main.py
+++ tests/test_main.py
@@ -206,11 +206,48 @@ class TestAccelerateFlex:
                 "career_outlook": "Graduated and in the workforce",
                 "heard_about": "Instructor/Professor",
             })
+            
+    @pytest.mark.integration
+    def test_accelerate_flex_schema_accepts_valid_insert(self, real_mongo_db: pymongo.database.Database):
+        """Integration test validating that an accelerate_flex document will be inserted
+        if it follows the collection json validation schema.
+        """
+        accelerate_flex_collection = real_mongo_db.get_collection(ACCELERATE_FLEX_COLLECTION)
+        prev_count = accelerate_flex_collection.count_documents({})
+        cti_id = 12345
+
+        # todo: upon a POST /api/accelerate-flex endpoint creation, this direct insert can be replaced with a TestClient call
+        insert_result = accelerate_flex_collection.insert_one({
+            "cti_id": cti_id,
+            "selected_deep_work": DeepWork(
+                day="Monday",
+                time="2pm - 4pm",
+                sprint="Spring 2024"
+            ).model_dump(),
+            "academic_goals": ["Bachelor's Degree", "Associate's Degree"],
+            "phone": "(800) 123-4567",
+            "academic_year": "Sophomore",
+            "grad_year": 2027,
+            "summers_left": 2,
+            "cs_exp": False,
+            "cs_courses": ["Data Structures", "Algorithms"],
+            "math_courses": ["Calculus 1A", "Discrete Math"],
+            "program_expectation": "Summer tech internship",
+            "career_outlook": "Graduated and in the workforce",
+            "heard_about": "Instructor/Professor",
+        })
+
+        assert insert_result.acknowledged
+
+        found_accelerate_flex = accelerate_flex_collection.find_one({"_id": insert_result.inserted_id})
+        assert found_accelerate_flex is not None
+        assert found_accelerate_flex["cti_id"] == cti_id
+        assert prev_count + 1 == accelerate_flex_collection.count_documents({})
 
 class TestPathwayGoals:
     @pytest.mark.integration
     def test_pathway_goals_schema_rejects_invalid_insert(self, real_mongo_db: pymongo.database.Database):
-        """Integration test validating that an pathway_goals document will not be inserted
+        """Integration test validating that a pathway_goals document will not be inserted
         if it does not follow the collection json validation schema.
         
         Relevant for inserts performed outside of API.
@@ -223,3 +260,28 @@ class TestPathwayGoals:
                 "pathway_desc": "Obtain a summer tech internship for 2025",
                 "course_req": "101A" # needs to be an array of strings
             })
+
+    @pytest.mark.integration
+    def test_pathway_goals_schema_accepts_valid_insert(self, real_mongo_db: pymongo.database.Database):
+        """Integration test validating that a pathway_goals document will be inserted
+        if it follows the collection json validation schema.
+        """
+        pathway_goals_collection = real_mongo_db.get_collection(PATHWAY_GOALS_COLLECTION)
+        prev_count = pathway_goals_collection.count_documents({})
+        pathway_goal = "Summer Tech Internship 2025"
+        pathway_desc = "Obtain a summer tech internship for 2025"
+        course_req = ["101A", "202A"]
+
+        # todo: upon a POST /api/pathway-goals endpoint creation, this direct insert can be replaced with a TestClient call
+        insert_result = pathway_goals_collection.insert_one({
+            "pathway_goal": pathway_goal,
+            "pathway_desc": pathway_desc,
+            "course_req": course_req
+        })
+
+        assert insert_result.acknowledged
+
+        found_accelerate_flex = pathway_goals_collection.find_one({"_id": insert_result.inserted_id})
+        assert found_accelerate_flex is not None
+        assert found_accelerate_flex["pathway_goal"] == pathway_goal
+        assert prev_count + 1 == pathway_goals_collection.count_documents({})
