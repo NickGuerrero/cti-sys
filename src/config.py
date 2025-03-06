40df57b532dee5593afcafee380187eeb8698a2b
Conlyn Pattison <cpattison@csumb.edu>
2025-03-05 16:15:35 -0800

    Add Accelerate Flex, Pathway Goals, & Courses Collections (#19)
    * Add fields to pydantic models & schema properties to collections
    
    * Add pydantic model unit tests
    
    Changes the definition of PathwayGoals and Couse base models to not
    allow for extra fields to better reflect design doc. Adds integration
    tests for 'courses' collection schema acceptances and rejections. Adds
    unit tests to account for expected success, failure, and results of
    pydantic model validations for these new collection documents. These are
    done to account for a lack existing endpoints.

--- src/config.py
+++ src/config.py
@@ -4,3 +4,6 @@
 MONGO_DATABASE_NAME = "cti_mongo_db"
 
 APPLICATIONS_COLLECTION = "applications"
+ACCELERATE_FLEX_COLLECTION = "accelerate_flex"
+PATHWAY_GOALS_COLLECTION = "pathway_goals"
+COURSES_COLLECTION = "courses"
