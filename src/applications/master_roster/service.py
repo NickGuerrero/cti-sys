a10984f2800fcc501916aeba7a353b294ad6c2c4
ConlynPattison <cpattison@csumb.edu>
2025-06-29 22:42:41 -0700

    Resolved majority of processes to functions

--- src/applications/master_roster/service.py
+++ src/applications/master_roster/service.py
@@ -1,7 +1,8 @@
-from typing import Any, List
+from typing import Any, List, Tuple
 from fastapi import HTTPException
 from pydantic import ValidationError
 from pymongo import UpdateOne
+from pymongo.collection import Collection
 from pymongo.database import Database as MongoDatabase
 from requests import get
 from sqlalchemy import select
@@ -19,7 +20,55 @@ def create_master_roster_records(
     mongo_db: MongoDatabase,
     postgres_session: Session
 ):
-    # fetch QuizSubmissions from Canvas for Commitment Quiz (TODO w/ pagination)
+    user_submissions_dict = get_all_quiz_submissions()
+
+    application_collection = mongo_db.get_collection(APPLICATIONS_COLLECTION)
+
+    applications_dict, invalid_applications_count = get_valid_applications(
+        application_collection=application_collection,
+        user_submissions_dict=user_submissions_dict
+    )
+
+    update_applicant_docs_commitment_status(
+        application_collection=application_collection,
+        applications_dict=applications_dict
+    )
+    
+    duplicate_ids_count = remove_duplicate_applicants(
+        applications_dict=applications_dict,
+        postgres_session=postgres_session
+    )
+
+    # add each remaining applications_dict value as a Student record TODO try wrap
+    students = [application_to_student(app) for app in applications_dict.values()]
+    postgres_session.add_all(students)
+    postgres_session.flush()
+
+    create_applicant_flex_documents(
+        applications_dict=applications_dict,
+        mongo_db=mongo_db,
+        postgres_session=postgres_session
+    )
+    
+    update_applicant_docs_master_added(
+        applications_dict=applications_dict,
+        application_collection=application_collection,
+        postgres_session=postgres_session
+    )
+
+    postgres_session.commit()
+    return MasterRosterCreateResponse(
+        status=201,
+        message=f"Successfully added users to Master Roster" # TODO desc spec
+    )
+
+def get_all_quiz_submissions() -> dict[int, QuizSubmission]:
+    """
+    Function fetches Commitment Quiz QuizSubmissions from Canvas.
+
+    Uses multiple external requests through pagination to fetch all submissions.
+    """
+    # TODO pagination
     url = f"{settings.canvas_api_url}/api/v1/courses/{settings.course_id_101}/quizzes/{settings.commitment_quiz_id}/submissions"
     headers = {
         "Authorization": f"Bearer {get_canvas_access_token()}",
@@ -47,8 +96,20 @@ def create_master_roster_records(
             message="No commitment quiz submissions to process"
         )
     
-    # fetch the applications for the newly committed students
-    application_collection = mongo_db.get_collection(APPLICATIONS_COLLECTION)
+    return user_submissions_dict
+
+def get_valid_applications(
+    application_collection: Collection,
+    user_submissions_dict: dict[int, QuizSubmission]
+) -> Tuple[dict[int, ApplicationWithMasterProps], int]:
+    """
+    Function fetches Application collection documents from MongoDB.
+
+    Result is a tuple with first index being a map of valid applications and second index
+    containing the count of invalid documents found which met the DB filter criteria.
+
+    `-> (dict, invalid_count)`
+    """
     application_documents = application_collection.find({
         "$and": [
             {"canvas_id": {"$in": list(user_submissions_dict.keys())}},
@@ -62,11 +123,18 @@ def create_master_roster_records(
     for app_doc in application_documents:
         try:
             app = ApplicationWithMasterProps(**app_doc)
-            applications_dict[app.canvas_id] = app # NOTE technically possible for duplicates here
+            applications_dict[app.canvas_id] = app
         except ValidationError as e:
             # NOTE log details when able
             invalid_applications_count += 1
 
+
+def update_applicant_docs_commitment_status(
+    application_collection: Collection,
+    applications_dict: dict[int, ApplicationWithMasterProps]
+) -> None:
+    """
+    """
     # update application documents to reflect a submitted commitment quiz
     application_updates: List[UpdateOne] = []
     for user_canvas_id, _ in applications_dict.items():
@@ -84,7 +152,13 @@ def create_master_roster_records(
             status_code=500,
             detail=f"Database failed to acknowledge bulk applications commitment update"
         )
-    
+
+def remove_duplicate_applicants(
+    applications_dict: dict[int, ApplicationWithMasterProps],
+    postgres_session: Session
+) -> int:
+    """
+    """
     # validate that the users have not committed already from PostreSQL -> else don't process again
     select_student_ids_stmt = (
         select(Student.cti_id)
@@ -105,87 +179,79 @@ def create_master_roster_records(
             detail=f"No new commitments, {duplicate_ids_count} duplicate requests"
         )
 
-    # add each remaining applications_dict value as a Student record TODO try wrap
-    students: list[Student] = []
-    for id, application in applications_dict.items():
-        student = Student(
-            cti_id=application.canvas_id,
-            fname=application.fname,
-            pname=application.pname,
-            lname=application.lname,
-            # 'join_date' default set by DB
-            target_year=application.app_submitted.year, # FIXME change to base on month and year
-            gender=application.gender,
-            first_gen=application.first_gen,
-            institution=application.institution,
-            # 'is_graduate' default set
-            birthday=application.birthday,
-            # ca_region TODO this is missing from the DB, notes suggest not needed
-            # 'active' default set
-            # 'cohort_lc' default set
-            # launch TODO this is missing from the DB, notes suggest not needed
-            email_addresses=[
-                StudentEmail(
-                    email=application.email,
-                    cti_id=application.canvas_id,
-                    is_primary=True,
-                )
-            ],
-            canvas_id=CanvasID(canvas_id=application.canvas_id, cti_id=application.canvas_id),
-            ethnicities=[
-                Ethnicity(
-                    cti_id=application.canvas_id,
-                    ethnicity=ethnicity,
-                ) for ethnicity in application.race_ethnicity
-            ],
-            accelerate_record=Accelerate( # NOTE is this needed?
+    return duplicate_ids_count
+
+def application_to_student(application: ApplicationWithMasterProps) -> Student:
+    student = Student(
+        cti_id=application.canvas_id,
+        fname=application.fname,
+        pname=application.pname,
+        lname=application.lname,
+        # 'join_date' default set by DB
+        target_year=application.app_submitted.year, # FIXME change to base on month and year
+        gender=application.gender,
+        first_gen=application.first_gen,
+        institution=application.institution,
+        # 'is_graduate' default set
+        birthday=application.birthday,
+        # ca_region TODO this is missing from the DB, notes suggest not needed
+        # 'active' default set
+        # 'cohort_lc' default set
+        # launch TODO this is missing from the DB, notes suggest not needed
+        email_addresses=[
+            StudentEmail(
+                email=application.email,
                 cti_id=application.canvas_id,
-                # 'student_type' default set
-                # accountability_group
-                # accountability_team
-                # pathway_goal
-                # 'participation_score' default set
-                # 'sessions_attended' default set
-                # 'participation_streak' default set
-                returning_student=application.returning,
-                # 'inactive_weeks' default set
-                # 'active' default set
-            ),
-        )
-        students.append(student)
-
-    # NOTE all or nothing add + flush; duplicates search and data validation done above
-    postgres_session.add_all(students)
-    postgres_session.flush()
-
-    flex_model_dicts: list[dict[str, Any]] = []
-    for id, application in applications_dict.items():
-        accelerate_flex = AccelerateFlexBase(
+                is_primary=True,
+            )
+        ],
+        canvas_id=CanvasID(canvas_id=application.canvas_id, cti_id=application.canvas_id),
+        ethnicities=[
+            Ethnicity(
+                cti_id=application.canvas_id,
+                ethnicity=ethnicity,
+            ) for ethnicity in application.race_ethnicity
+        ],
+        accelerate_record=Accelerate(
             cti_id=application.canvas_id,
-            academic_goals=application.academic_goals,
-            phone=application.phone,
-            academic_year=application.academic_year,
-            grad_year=application.grad_year,
-            summers_left=application.summers_left,
-            cs_exp=application.cs_exp,
-            cs_courses=application.cs_courses,
-            math_courses=application.math_courses,
-            program_expectation=application.program_expectation,
-            career_outlook=application.career_outlook,
-            heard_about=application.heard_about
-        )
-        flex_model_dicts.append(accelerate_flex.model_dump())
+            # 'student_type' default set
+            # accountability_group
+            # accountability_team
+            # pathway_goal
+            # 'participation_score' default set
+            # 'sessions_attended' default set
+            # 'participation_streak' default set
+            returning_student=application.returning,
+            # 'inactive_weeks' default set
+            # 'active' default set
+        ),
+    )
+    return student
 
-    accelerate_flex_collection = mongo_db.get_collection(ACCELERATE_FLEX_COLLECTION)
-    insert_result = accelerate_flex_collection.insert_many(flex_model_dicts)
-    if not insert_result.acknowledged:
-        postgres_session.rollback()
-        raise HTTPException(
-            status_code=500,
-            detail=f"Database failed to acknowledge flex insert for user with cti_id {application.canvas_id}"
-        )
-    
-    # update application documents to reflect a submitted commitment quiz
+def application_to_flex(application: ApplicationWithMasterProps) -> AccelerateFlexBase:
+    accelerate_flex = AccelerateFlexBase(
+        cti_id=application.canvas_id,
+        academic_goals=application.academic_goals,
+        phone=application.phone,
+        academic_year=application.academic_year,
+        grad_year=application.grad_year,
+        summers_left=application.summers_left,
+        cs_exp=application.cs_exp,
+        cs_courses=application.cs_courses,
+        math_courses=application.math_courses,
+        program_expectation=application.program_expectation,
+        career_outlook=application.career_outlook,
+        heard_about=application.heard_about
+    )
+    return accelerate_flex
+
+def update_applicant_docs_master_added(
+        applications_dict: dict[int, ApplicationWithMasterProps],
+        application_collection: Collection,
+        postgres_session: Session
+):
+    """
+    """
     application_updates: List[UpdateOne] = []
     for user_canvas_id, _ in applications_dict.items():
         application_updates.append(
@@ -198,15 +264,26 @@ def create_master_roster_records(
         )
     
     if not application_collection.bulk_write(application_updates).acknowledged:
-        # TODO do we need to require a sync with postgres_session.rollback() ?
+        postgres_session.rollback()
         raise HTTPException(
             status_code=500,
             detail=f"Database failed to acknowledge bulk applications master_added update"
         )
 
-    # if no errors occurred, commit the session transaction and respond with success status + message
-    postgres_session.commit()
-    return MasterRosterCreateResponse(
-        status=201,
-        message=f"Successfully added users to Master Roster" # TODO desc spec
-    )
+def create_applicant_flex_documents(
+    applications_dict: dict[int, ApplicationWithMasterProps],
+    mongo_db: MongoDatabase,
+    postgres_session: Session
+):
+    """
+    """
+    flex_model_dicts = [application_to_flex(app).model_dump() for app in applications_dict.values()]
+
+    accelerate_flex_collection = mongo_db.get_collection(ACCELERATE_FLEX_COLLECTION)
+    insert_result = accelerate_flex_collection.insert_many(flex_model_dicts)
+    if not insert_result.acknowledged:
+        postgres_session.rollback()
+        raise HTTPException(
+            status_code=500,
+            detail=f"Database failed to acknowledge flex inserts"
+        )
