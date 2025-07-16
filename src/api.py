900163b6e21627e97ddb79254ff1c59ed6aba97e
arsalaerfan <erfanarsala831@gmail.com>
2025-07-16 09:43:47 -0700

    Fix typo in attendance log router import

--- src/api.py
+++ src/api.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from src.applications.router import router as applications_router
 from src.applications.canvas_export.router import router as canvas_export_router
 from src.students.alternate_emails.router import router as student_alternate_emails_router
-from src.students.attendance_log.router import router as student_attendence_log_router
+from src.students.attendance_log.router import router as student_attendance_log_router
 from src.students.accelerate.process_attendance.router import router as accelerate_attendance_record_router
 from src.students.missing_students.router import router as student_recover_attendance_router
 
@@ -33,7 +33,7 @@ api_router.include_router(
 
 # /api/students/process-attendance-log...
 api_router.include_router(
-    student_attendence_log_router,
+    student_attendance_log_router,
     prefix="/students/process-attendance-log",
     tags=["Students"]
 )
