c653aa58f305b27c07e383bb36e84435a407d9ee
Nicolas Guerrero <Guerreronicolas1872@gmail.com>
2025-08-12 16:24:13 -0700

    Rename env vars to avoid leading numeral

--- src/config.py
+++ src/config.py
@@ -7,11 +7,11 @@ class Settings(BaseSettings):
     # NOTE SIS section ids need to be manually defined in Canvas
     # Go to Settings -> Sections -> Your Section -> Set SIS ID
     app_env: str = os.getenv("APP_ENV")
-    unterview_course_id: int = os.getenv("UNTERVIEW_COURSE_ID")
+    unterview_course_id: int = os.getenv("COURSE_ID_UNTERVIEW")
     unterview_sis_course_id: str = os.getenv("UNTERVIEW_SIS_COURSE_ID")
     current_unterview_sis_section_id: str = os.getenv("CUR_UNTERVIEW_SIS_SECTION_ID")
     current_unterview_section_id: int = os.getenv("CUR_UNTERVIEW_SECTION_ID")
-    course_id_101: int = os.getenv("101_COURSE_ID")
+    course_id_101: int = os.getenv("COURSE_ID_101")
     commitment_quiz_id: int = os.getenv("COMMITMENT_QUIZ_ID")
     canvas_api_url: str = "https://cti-courses.test.instructure.com"
     canvas_api_test_url: str = "https://cti-courses.test.instructure.com"
