aa34db8584750b30a6a39e6f112ae0675b94244a
ConlynPattison <cpattison@csumb.edu>
2025-08-13 15:01:29 -0700

    Fix getenv None result and str cast

--- src/config.py
+++ src/config.py
@@ -1,18 +1,19 @@
 import os
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
-# Consider moving into a pydantic-settings package's `class Settings(BaseSettings)` for environment-specific overrides
+load_dotenv()
 
 class Settings(BaseSettings):
     # NOTE SIS section ids need to be manually defined in Canvas
     # Go to Settings -> Sections -> Your Section -> Set SIS ID
     app_env: str = os.getenv("APP_ENV")
-    unterview_course_id: int = os.getenv("COURSE_ID_UNTERVIEW")
+    unterview_course_id: int = int(os.getenv("COURSE_ID_UNTERVIEW"))
     unterview_sis_course_id: str = os.getenv("UNTERVIEW_SIS_COURSE_ID")
     current_unterview_sis_section_id: str = os.getenv("CUR_UNTERVIEW_SIS_SECTION_ID")
-    current_unterview_section_id: int = os.getenv("CUR_UNTERVIEW_SECTION_ID")
-    course_id_101: int = os.getenv("COURSE_ID_101")
-    commitment_quiz_id: int = os.getenv("COMMITMENT_QUIZ_ID")
+    current_unterview_section_id: int = int(os.getenv("CUR_UNTERVIEW_SECTION_ID"))
+    course_id_101: int = int(os.getenv("COURSE_ID_101"))
+    commitment_quiz_id: int = int(os.getenv("COMMITMENT_QUIZ_ID"))
     canvas_api_url: str = "https://cti-courses.test.instructure.com"
     canvas_api_test_url: str = "https://cti-courses.test.instructure.com"
 
