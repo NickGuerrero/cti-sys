ed22dd56a7053f7f0662ec1753210539261f1113
arsalaerfan <erfanarsala831@gmail.com>
2025-08-28 13:38:27 -0700

    Refactor settings configuration to use SettingsConfigDict

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
 from typing import Optional
 
@@ -10,23 +10,22 @@ class Settings(BaseSettings):
     current_unterview_section_id: int = 194
     canvas_api_url: str = "https://cti-courses.test.instructure.com"
     canvas_api_test_url: str = "https://cti-courses.test.instructure.com"
-    
-    # SA's Sheet URL for allowed emails and form submissions password
-    allowed_sas_sheet_url: Optional[str] = Field(None, env="ALLOWED_SAS_SHEET_URL")
-    attendance_password: Optional[str] = Field(None, env="ATTENDANCE_PASSWORD")
-    attendance_api_key: Optional[str] = Field(None, env="ATTENDANCE_API_KEY")
 
-    model_config = {
-        "case_sensitive": False, # Make environment variable names case-insensitive
-        "env_file": ".env", # Load environment variables from .env file
-        "extra": "ignore", # Ignore any extra fields not defined in the model
-    }
+    # SA's Sheet URL for allowed emails and form submissions password
+    allowed_sas_sheet_url: Optional[str] = Field(default=None, validation_alias="ALLOWED_SAS_SHEET_URL")
+    attendance_password: Optional[str] = Field(default=None, validation_alias="ATTENDANCE_PASSWORD")
+    attendance_api_key: Optional[str] = Field(default=None, validation_alias="ATTENDANCE_API_KEY")
+
+    model_config = SettingsConfigDict(
+        case_sensitive = False, # Make environment variable names case-insensitive
+        env_file = ".env", # Load environment variables from .env file
+        extra = "ignore", # Ignore any extra fields not defined in the model
+    )
 
 settings = Settings()
 
 # MongoDB
 MONGO_DATABASE_NAME = "cti_mongo_db"
-
 APPLICATIONS_COLLECTION = "applications"
 ACCELERATE_FLEX_COLLECTION = "accelerate_flex"
 PATHWAY_GOALS_COLLECTION = "pathway_goals"
