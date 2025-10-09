dec3aa25fb7c9c03d1bc00cc2b413c8fedc7178c
arsalaerfan <erfanarsala831@gmail.com>
2025-10-09 15:40:48 -0700

    Refactor FastAPI application to disable health check endpoints and /docs for production env

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,17 @@
 from fastapi.testclient import TestClient
-
+from src.config import settings
+import pytest
 from src.main import app
 
 client = TestClient(app)
 
+@pytest.mark.skipif(settings.app_env == "production", reason="Health check route")
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "cti-sys v1.0.0"}
 
+@pytest.mark.skipif(settings.app_env == "production", reason="Health check route")
 def test_confirm_conn():
     response = client.get("/test-connection")
     assert response.status_code == 200
