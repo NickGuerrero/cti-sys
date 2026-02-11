da924be1e07469105a6b8a5a3771de44255e7305
Nicolas Guerrero <Guerreronicolas1872@gmail.com>
2026-02-11 01:05:02 -0800

    Remove direct env calls, use the config.py settings

--- tests/gsheet/test_gsheet.py
+++ tests/gsheet/test_gsheet.py
@@ -1,5 +1,4 @@
 import pytest
-from os import environ
 import pandas
 from datetime import date
 
@@ -14,10 +13,10 @@ class TestGSheetGroup:
     @pytest.mark.integration
     @pytest.mark.gsheet
     def testGroupAttendance(self, monkeypatch, client):
-        monkeypatch.setenv("ROSTER_SHEET_KEY", environ.get("TEST_SHEET_KEY"))
+        monkeypatch.setenv("ROSTER_SHEET_KEY", settings.test_sheet_key)
         response = client.post("/api/gsheet/group-attendance",
                                params={
-                                   "spreadsheet_id": environ.get("TEST_SHEET_KEY"),
+                                   "spreadsheet_id": settings.test_sheet_key,
                                    "spreadsheet_name": "Test Group", # You can change this later to proper spreadsheet name
                                    "start_date": date(2025, 10, 1),
                                    "end_date": date(2025, 10, 15)
@@ -26,12 +25,12 @@ class TestGSheetGroup:
         assert response.status_code == 201
 
         gc = utils.create_credentials()
-        output_spreadsheet = gc.open_by_key(environ.get("TEST_SHEET_KEY"))
+        output_spreadsheet = gc.open_by_key(settings.test_sheet_key)
         # Create a sheet in the spreadsheet called Test Group to test the endpoint.
         output_worksheet = output_spreadsheet.worksheet("Test Group")
         output_df = pandas.DataFrame(output_worksheet.get_all_records())
 
-        cti_ids = service.fetch_cti_ids_from_sheet(environ.get("TEST_SHEET_KEY"), "Test Group", gc)
+        cti_ids = service.fetch_cti_ids_from_sheet(settings.test_sheet_key, "Test Group", gc)
         attendance_data = service.fetch_group_attendance(CONN, date(2025, 10, 1), date(2025, 10, 15), cti_ids)
 
         # Note that modifying the test sheet during the test will break the assertion
@@ -41,17 +40,17 @@ class TestGSheetGroup:
     @pytest.mark.integration
     @pytest.mark.gsheet
     def testDefaultLookbackDays(self, monkeypatch, client):
-        monkeypatch.setenv("ROSTER_SHEET_KEY", environ.get("TEST_SHEET_KEY"))
+        monkeypatch.setenv("ROSTER_SHEET_KEY", settings.test_sheet_key)
         response = client.post("/api/gsheet/group-attendance",
                                params={
-                                   "spreadsheet_id": environ.get("TEST_SHEET_KEY"),
+                                   "spreadsheet_id": settings.test_sheet_key,
                                    "spreadsheet_name": "Test Group", # You can change this later to proper spreadsheet name
                                },
                             )
         assert response.status_code == 201
 
         gc = utils.create_credentials()
-        output_spreadsheet = gc.open_by_key(environ.get("TEST_SHEET_KEY"))
+        output_spreadsheet = gc.open_by_key(settings.test_sheet_key)
         # Create a sheet in the spreadsheet called Test Group to test the endpoint.
         output_worksheet = output_spreadsheet.worksheet("Test Group")
         output_df = pandas.DataFrame(output_worksheet.get_all_records())
@@ -59,7 +58,7 @@ class TestGSheetGroup:
         end_date = date.today()
         start_date = end_date - timedelta(days=settings.default_attendance_lookback_days)
 
-        cti_ids = service.fetch_cti_ids_from_sheet(environ.get("TEST_SHEET_KEY"), "Test Group", gc)
+        cti_ids = service.fetch_cti_ids_from_sheet(settings.test_sheet_key, "Test Group", gc)
         attendance_data = service.fetch_group_attendance(CONN, start_date, end_date, cti_ids)
 
         # Note that modifying the test sheet during the test will break the assertion
